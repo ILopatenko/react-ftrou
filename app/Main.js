c7c69db3862efd1c6d1d0698aab8c46a39fd8a4f
Iurii Lopatenko <yalopatenko@gmail.com>
2021-07-25 23:33:43 -0700

    END of CH5

--- app/Main.js
+++ app/Main.js
@@ -1,30 +1,49 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import Footer from "./components/Footer"
-import Header from "./components/Header"
-import HomeGuest from "./components/HomeGuest"
-import About from "./components/About"
-import Terms from "./components/Terms"
-import { BrowserRouter, Switch, Route } from "react-router-dom"
-
-const Main = () => (
-  <BrowserRouter>
-    <Header />
-    <Switch>
-      <Route path="/" exact>
-        <HomeGuest />
-      </Route>
-      <Route path="/about" exact>
-        <About />
-      </Route>
-      <Route path="/terms" exact>
-        <Terms />
-      </Route>
-    </Switch>
-    <Footer />
-  </BrowserRouter>
-)
-ReactDOM.render(<Main />, document.querySelector("#app"))
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import HomeGuest from "./components/HomeGuest";
+import About from "./components/About";
+import Terms from "./components/Terms";
+import CreatePost from "./components/CreatePost";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import axios from "axios";
+import ViewSinglePost from "./components/ViewSinglePost";
+import FlashMessages from "./components/FlashMessages";
+axios.defaults.baseURL = "http://localhost:8080";
+const Main = () => {
+  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexAppToken")));
+  const [flashMessages, setFlashMessages] = useState([]);
+  const addFlashMessage = (msg) => {
+    setFlashMessages((prev) => prev.concat(msg));
+  };
+  return (
+    <BrowserRouter>
+      <FlashMessages messages={flashMessages} />
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+      <Switch>
+        <Route path="/" exact>
+          {loggedIn ? <Home /> : <HomeGuest />}
+        </Route>
+        <Route path="/about" exact>
+          <About />
+        </Route>
+        <Route path="/create-post" exact>
+          <CreatePost addFlashMessage={addFlashMessage} />
+        </Route>
+        <Route path="/post/:id" exact>
+          <ViewSinglePost />
+        </Route>
+        <Route path="/terms" exact>
+          <Terms />
+        </Route>
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+ReactDOM.render(<Main />, document.querySelector("#app"));
 if (module.hot) {
-  module.hot.accept
+  module.hot.accept;
 }
---
+++ app/components/HeaderLoggedIn.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const HeaderLoggedIn = (props) => {
+  const handleLoguot = () => {
+    props.setLoggedIn(false);
+    localStorage.removeItem("complexAppToken");
+    localStorage.removeItem("complexAppUsername");
+    localStorage.removeItem("complexAppAvatar");
+  };
+  return (
+    <div className="flex-row my-3 my-md-0">
+      <a href="#" className="text-white mr-2 header-search-icon">
+        <i className="fas fa-search"></i>
+      </a>
+      <span className="mr-2 header-chat-icon text-white">
+        <i className="fas fa-comment"></i>
+        <span className="chat-count-badge text-white"> </span>
+      </span>
+      <a href="#" className="mr-2">
+        <img className="small-header-avatar" src={localStorage.getItem("complexAppAvatar")} />
+      </a>
+      <Link to="/create-post" className="btn btn-sm btn-success mr-2">
+        Create Post
+      </Link>
+      <button onClick={handleLoguot} className="btn btn-sm btn-secondary">
+        Sign Out
+      </button>
+    </div>
+  );
+};
+export default HeaderLoggedIn;
